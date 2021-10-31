76684f4aedae5641de9684cbfd95c7acd8fd36b2
kolikay <kolikay1989@gmail.com>
2021-10-31 18:16:57 +0100

    completed add task sreen

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,20 +1,28 @@
+// ignore_for_file: recursive_getters
+
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List <Task> task = [
+  final List<Task> _task = [
     Task(name: 'bay bread'),
     Task(name: 'buy milk'),
     Task(name: 'buy food')
   ];
 
   int get taskCount {
-    return task.length;
+    return _task.length;
+  }
+
+  UnmodifiableListView<Task> get task {
+    return UnmodifiableListView(_task);
   }
 
   void addTask(String newTaskTitle) {
     final newTask = Task(name: newTaskTitle);
-    task.add(newTask);
+    _task.add(newTask);
     notifyListeners();
   }
 }
